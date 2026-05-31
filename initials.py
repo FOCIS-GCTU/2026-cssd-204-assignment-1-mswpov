112b14bb93cc819d931ec8ab992ec77a59d7c680
Marian <abenaaa.x@gmail.com>
2026-05-31 10:53:19 +0000

    Finished my initials

--- initials.py
+++ initials.py
@@ -1,7 +1,39 @@
+#File: initials.py
+#Description: This program serves to print out my initials in block letters.
+#Assignment Number:1
+#
+#Name: Marian Maami Nana Whyte
+#STUDENT ID: 2425402381
+#Email: 2425402381@live.gctu.edu.gh
+#Grader:Augustus
+#
+#On my honor, Marian Maami Nana Whyte, this programming assignment is my own work
+#and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Prints out my initials in block letters.
 
+    print()
+    print("...MMNW")
+    print()
 
+    print("...MMMM....MMMM........MMMM....MMMM........NNNN......NN........WW...WW...WW.....")
+    print("...MMMM....MMMM........MMMM....MMMM........NNNN......NN........WW...WW...WW.....")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM........NN.NN.....NN........WW...WW...WW.....")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM........NN..NN....NN........WW..WWWW..WW.....")
+    print("...MM.MM..MM.MM........MM.MM..MM.MM........NN...NN...NN........WW..WWWW..WW.....")
+    print("...MM..MMMM..MM........MM..MMMM..MM........NN....NN..NN........WW.WW..WW.WW.....")
+    print("...MM..MMMM..MM........MM..MMMM..MM........NN.....NN.NN........WW.WW..WW.WW.....")
+    print("...MM...MM...MM........MM...MM...MM........NN......NNNN........WW.WW..WW.WW.....")
+    print("...MM...MM...MM...**...MM...MM...MM...**...NN.......NNN...**...WWWW....WWWW...**")
+    print("...MM...MM...MM...**...MM...MM...MM...**...NN.......NNN...**...WWWW....WWWW...**")
+           
+    print()
 
 main()
+
+
+          
+        
+        
